ad5c94fbd260037c4a14827fa7c1d3244a662835
zhanglicheng66 <405328994@qq.com>
2018-10-15 17:59:22 +0800

    Initial commit

---
+++ lunbojquery.js
@@ -0,0 +1,15 @@
+$(p1).on('click', function () {
+    $(images).css({
+        transform:'translateX(0)'
+    })
+})
+$(p2).on('click', function () {
+    $(images).css({
+        transform:'translateX(-580px)'
+    })
+})
+$(p3).on('click', function () {
+    $(images).css({
+        transform:'translateX(-1160px)'
+    })
+})
